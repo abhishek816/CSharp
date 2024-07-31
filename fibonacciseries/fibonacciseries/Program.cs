a8b3ef43f068e72660f4dcf53748a5259f022c89
abhishek816 <abhishekbkumar321@gmail.com>
2024-07-31 13:49:10 +0530

    Practice codes

---
+++ fibonacciseries/fibonacciseries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacciseries
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n, a = 0, b = 1,c;
+            Console.WriteLine("Enter the limit:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonacci series:");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            while(3<=n)
+            { 
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+                n = n - 1;
+
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
