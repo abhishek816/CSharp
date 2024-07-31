329fb3f99eaa049421e662800956ec7d6f7b8d63
abhishek816 <abhishekbkumar321@gmail.com>
2024-07-31 14:09:24 +0530

    31/07/2024

---
+++ Nnaturalnousingfor/Nnaturalnousingfor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nnaturalnousingfor
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n;
+            Console.WriteLine("Enter the limit");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Natural Numbers are:");
+            for (int i=1; i<=n;i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
