f31e233a1633f131278db086b4e3a50799a42cdd
abhishek816 <abhishekbkumar321@gmail.com>
2024-07-31 14:19:31 +0530

    30/07/2024

---
+++ factorialusingfor/factorialusingfor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorialusingfor
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n,i,p=1;
+            Console.WriteLine("Enter the number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Factorial of {0} are:",n);
+            for(i=1;i<=n;i++)
+            {
+                p = p * i;
+            }
+            Console.WriteLine(p);
+            Console.ReadKey();
+        }
+    }
+}
