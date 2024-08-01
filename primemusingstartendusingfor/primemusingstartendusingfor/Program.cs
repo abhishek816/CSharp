72a4ddafb0a0508d0b93d81e8dfbbe09f36df1c6
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-01 13:40:27 +0530

    1/08/2024

---
+++ primemusingstartendusingfor/primemusingstartendusingfor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primemusingstartendusingfor
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int start, end;
+            Console.WriteLine("Enter starting number:");
+            start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter ending number:");
+            end = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Prime Number from {0} to {1} are:", start, end);
+            for(int j=start;j<=end;j++)
+            {
+                int f = 0;
+                for(int i=1;i<=j;i++)
+                {
+                    if (j % i == 0) 
+                    {
+                        f = f + 1;
+                    }
+                }
+                if(f==2)
+                {
+                    Console.WriteLine(j);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ primeuptonusingfor/primeuptonusingfor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeuptonusingfor
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n;
+            Console.WriteLine("Enter a number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Prime numbers upto {0} are:",n);
+            for (int j=1;j<=n;j++)
+            {
+                int f = 0;
+                for(int i=1;i<=j;i++)
+                {
+                    if (j % i == 0)
+                    {
+                        f = f + 1;
+                    }
+                }
+                if(f==2)
+                {
+                    Console.WriteLine( j);
+                }
+                
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
