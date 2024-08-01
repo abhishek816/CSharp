3aacb7df7891730cbfe5a35487be0e0f2293ae31
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-01 13:51:50 +0530

    01/08/2024

---
+++ stringreversal/stringreversal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringreversal
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string s,r="";
+            Console.WriteLine("Enter the string");
+            s = Console.ReadLine();
+            for (int i=s.Length-1;i>=0;i--)
+            {
+                r = r + s[i];
+            }
+            Console.WriteLine("Reverse is {0}", r);
+            Console.ReadKey();
+        }
+    }
+}
