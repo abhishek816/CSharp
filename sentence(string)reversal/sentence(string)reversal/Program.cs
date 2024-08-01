a46194572deb77d78c2c7439618c79fb1ab7c70c
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-01 14:19:35 +0530

    01/08/2024

---
+++ sentence(string)reversal/sentence(string)reversal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sentence_string_reversal
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string s, re = " ";
+            Console.WriteLine("Enter a string:");
+            s = Console.ReadLine();
+            string[] ar = s.Split();
+            for (int i=ar.Length-1;i>=0;i--)
+            {
+                re = re + ar[i] + " ";
+            }
+            Console.WriteLine("Reversed string:{0}", re);
+            Console.ReadKey();
+        }
+    }
+}
