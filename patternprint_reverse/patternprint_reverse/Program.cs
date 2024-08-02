91f936aef80a60971db4e9ea1637ad17f4f1158d
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-02 13:36:18 +0530

    02/08/2024

---
+++ patternprint_reverse/patternprint_reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patternprint_reverse
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n;
+            Console.WriteLine("Enter the limit:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*" + " ");
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
