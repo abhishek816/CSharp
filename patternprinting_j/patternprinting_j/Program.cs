79b933905ab77c764f09e900c2ab414ef5801979
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-02 13:07:47 +0530

    01/08/2024

---
+++ patternprinting_j/patternprinting_j/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patternprinting_j
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n;
+            Console.WriteLine("Enter the limit:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
