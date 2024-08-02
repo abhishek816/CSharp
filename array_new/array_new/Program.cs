351040254df972b0d92415cb48c140f732dcb412
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-02 14:53:32 +0530

    02/08/2024

---
+++ array_new/array_new/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_new
+{
+    class Program
+    {
+       public static void Main()
+        {
+            int n;
+            Console.WriteLine("Enter the limit:");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] ar = new int[n];
+            Console.WriteLine("Enter the array elments:");
+            for(int i=0;i<n;i++)//index
+            {
+                ar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Array elements are:");
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine(ar[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
