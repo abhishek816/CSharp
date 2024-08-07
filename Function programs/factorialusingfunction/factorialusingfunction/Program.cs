19cbdb6567a9e60d0db6606d2e308389277bfa38
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-07 14:38:59 +0530

    07/08/2024

---
+++ Function programs/factorialusingfunction/factorialusingfunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorialusingfunction
+{
+    class A
+    {
+        public int fact(int n)
+        {
+            int p=1;
+            for(int i=1;i<=n;i++)
+            {
+                p = p * i;
+                
+            }
+            return p;
+
+        }
+        public static void Main()
+        
+        {
+            int n,f;
+            Console.WriteLine("Enter the limit:");
+            n = Convert.ToInt32(Console.ReadLine());
+            A obj = new A();
+            f=obj.fact(n);
+            Console.WriteLine("Factorial is:{0}", f);
+            Console.ReadKey();
+
+        }
+    }
+}
