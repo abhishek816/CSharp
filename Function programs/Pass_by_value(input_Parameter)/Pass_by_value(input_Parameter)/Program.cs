de9bb5c4f19a624c4ae03b7e432f72160685dfbe
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-07 14:05:06 +0530

    07/08/2024

---
+++ Function programs/Pass_by_value(input_Parameter)/Pass_by_value(input_Parameter)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pass_by_value_input_Parameter_
+{
+    class A
+    {
+        public void display(int x)
+        {
+            x = x + 10;
+            Console.WriteLine("Value inside functiion is{0}", x);
+        }
+
+        public static void Main()
+        {
+            int x = 10;
+            A obj = new A();
+            obj.display(x);
+            Console.WriteLine("Value inside main function is{0}", x);
+            Console.ReadKey();
+        }
+    }
+}
