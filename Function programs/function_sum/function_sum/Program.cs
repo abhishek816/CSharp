003f2470d07f82f7b1dd63c8b67b068b7ebb74d3
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-07 13:32:59 +0530

    07/08/2024

---
+++ Function programs/function_sum/function_sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function_sum
+{
+    class A
+    {
+        public int sum()
+        {
+            int a, b, s;
+            Console.WriteLine("enter two numbers:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            s = a + b;
+            return s;
+        }
+        public static void Main()
+        {
+            A obj = new A();
+            int s1 = obj.sum();
+            Console.WriteLine("Sum is :{0}",s1);
+            Console.ReadKey();
+        }
+    }
+}
