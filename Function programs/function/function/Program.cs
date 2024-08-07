23dc6a4ad662def421a648f51beccae11c122245
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-07 13:19:28 +0530

    07/08/2024

---
+++ Function programs/function/function/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function
+{
+    class A
+    {
+        public void normal()
+        {
+            Console.WriteLine("Normal function");
+        }
+        public static void Main()
+        {
+            A ob = new A();
+            ob.normal();
+            Console.ReadKey();
+        }
+
+    }
+}
