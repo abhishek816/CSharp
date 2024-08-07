266cfc037fcaea05df077a68b74746302842d0cd
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-07 13:40:13 +0530

    07/08/2024

---
+++ Function programs/function_product/function_product/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function_product
+{
+    class A
+    {
+        public void product()
+        {
+            int a, b, p;
+            Console.WriteLine("Enter the two numbers:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            p = a * b;
+            Console.WriteLine("Product of {0} and {1} is:{2}", a, b,p);
+        }
+        public static void Main()
+        {
+            A obj = new A();
+            obj.product();
+            Console.ReadKey();
+        }
+    }
+}
