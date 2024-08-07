4f82f568a9ecef909ca38286c6a97a539de953cf
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-07 14:13:15 +0530

    07/08/2024

---
+++ Function programs/reference_paramter(pass_by_adress)/reference_paramter(pass_by_adress)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reference_paramter_pass_by_adress_
+{
+    class A
+    {
+        public void display(ref int x)
+        {
+            x = x + 10;
+            Console.WriteLine("Value inside functiion is{0}", x);
+        }
+
+        public static void Main()
+        {
+            int x = 10;
+            A obj = new A();
+            obj.display(ref x);
+            Console.WriteLine("Value inside main function is{0}", x);
+            Console.ReadKey();
+        }
+    }
+}
