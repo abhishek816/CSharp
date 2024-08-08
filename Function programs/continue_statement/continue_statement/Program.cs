9c4e4fd34ca6767ae5a94ca0f8692216ba2acec2
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-08 14:30:58 +0530

    08/08/2024

---
+++ Function programs/continue_statement/continue_statement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace continue_statement
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int a = 0;
+            while (a < 5)
+            {
+                a = a + 1;
+                if(a==3)
+                {
+                    continue;
+                }
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
