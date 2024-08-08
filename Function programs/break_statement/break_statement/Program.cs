0239cde9f322166da44384f0e378470fdd7c1f92
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-08 14:28:20 +0530

    08/08/2024

---
+++ Function programs/break_statement/break_statement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace break_statement
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int a = 1;
+            while(a<=5)
+            {
+                if(a==3)
+                {
+                    break;
+                }
+                Console.WriteLine(a);
+                a = a + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
