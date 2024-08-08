b49a658839a4ac5af4f8293a9af370440070dbcf
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-08 14:39:46 +0530

    08/08/2024

---
+++ Function programs/switch_statement/switch_statement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_statement
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int a = 0;
+            Console.WriteLine("Enter the option:");
+            a = Convert.ToInt32(Console.ReadLine());
+            switch(a)
+            {
+                case 1:Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                default:Console.WriteLine("Invalid");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
