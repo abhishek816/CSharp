988e4f48e007a690f5f2507f25ceeda400c25e73
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-08 13:39:21 +0530

    08/07/2024

---
+++ Function programs/product_and_sum_function/product_and_sum_function/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_and_sum_function
+{
+    class A
+    {
+        public int sum(int a,int b)
+        {
+           
+           
+            return (a+b);
+        }
+        public void product(int x,int y)
+        {
+            Console.WriteLine("Product is:{0}", x * y);
+        }
+        public static void Main()
+        {
+            int n,m;
+            Console.WriteLine("Enter the numbers:");
+            n= Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            
+            A obj = new A();
+            int c=obj.sum(n, m);
+            
+            Console.WriteLine("Sum is:{0}", c);
+            obj.product(n, m);
+            Console.ReadKey();
+        }
+    }
+}
