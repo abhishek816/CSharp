1cb13710beea31be1c150d2c94bc0b216c847bae
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-08 14:56:44 +0530

    08/08/2024

--- Function programs/switch_statement/switch_statement/Program.cs
+++ Function programs/switch_statement/switch_statement/Program.cs
@@ -10,21 +10,32 @@ namespace switch_statement
     {
         public static void Main()
         {
-            int a = 0;
-            Console.WriteLine("Enter the option:");
-            a = Convert.ToInt32(Console.ReadLine());
-            switch(a)
+        //  int a = 0;
+        //Console.WriteLine("Enter the option:");
+        //a = Convert.ToInt32(Console.ReadLine());
+        //switch(a)
+        //{
+        //  case 1:Console.WriteLine("Sunday");
+        //    break;
+        //case 2:
+        //  Console.WriteLine("Monday");
+        //break;
+        //case 3:
+        //  Console.WriteLine("Tuesday");
+        //
+        //goto case 1;
+        //    default:Console.WriteLine("Invalid");
+        //        break;
+        // }
+        check: Console.WriteLine("Enter the age above 10");
+            int ag = Convert.ToInt32(Console.ReadLine());
+            if(ag<=10)
             {
-                case 1:Console.WriteLine("Sunday");
-                    break;
-                case 2:
-                    Console.WriteLine("Monday");
-                    break;
-                case 3:
-                    Console.WriteLine("Tuesday");
-                    goto case 1;
-                default:Console.WriteLine("Invalid");
-                    break;
+                goto check;
+            }
+            else
+            {
+                Console.WriteLine("Age is :{0}", ag);
             }
             Console.ReadKey();
         }
