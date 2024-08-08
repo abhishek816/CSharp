4291f6475e8f524ff676c5b46fb3316100400c0f
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-08 14:12:31 +0530

    08/08/2024

---
+++ Function programs/output_parameter/output_parameter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace output_parameter
+{
+    class A
+    {
+        public int display(int a,out int b)
+        {
+            a = a + 100;
+            b = a + 100;
+            return a;
+        }
+        public static void Main()
+        {
+            A obj = new A();
+            int a = 100, b, c;
+            c = obj.display(a, out b);
+            Console.WriteLine("a={0} and b={1}", c, b);
+            Console.ReadKey();
+        }
+    }
+}
