f12e1b9abae274a5908988a0500d8aae897d374c
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-09 13:37:45 +0530

    09/08/2024

---
+++ Inheritance/inheritance_sum/inheritance_sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_sum
+{
+    class A
+    {
+        public int a, b;
+        public void read()
+        {
+            
+            Console.WriteLine("Enter 2 numbers:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+        }
+        
+    }
+    class B : A
+    {
+        public void sum()
+        {
+            Console.WriteLine("Sum:{0}", a+b);
+            
+        }
+        public static void Main()
+        {
+            B obj = new B();
+            obj.read();
+            obj.sum();
+            Console.ReadKey();
+        }
+    }
+
+}
