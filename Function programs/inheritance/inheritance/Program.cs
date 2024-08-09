daee79d9f30bbfe6d29e3dcd8644cb2ecc07ba24
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-09 13:26:18 +0530

    09/08/2024

---
+++ Function programs/inheritance/inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class A
+    {
+        public void displayA()
+        {
+            Console.WriteLine("Class A method");
+        }
+    }
+    class B : A
+    {
+        public void displayB()
+        {
+            Console.WriteLine("Class B method");
+        }
+        public static void Main()
+        {
+            B obj = new B();
+            obj.displayA();
+            obj.displayB();
+            Console.ReadKey();
+        }
+    }
+}
