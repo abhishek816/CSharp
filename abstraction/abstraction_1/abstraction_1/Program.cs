af7dab3aeaa8605d6eb0a397a6dcf6abdb091254
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-12 14:35:27 +0530

    12/08/2024

---
+++ abstraction/abstraction_1/abstraction_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstraction_1
+{
+    abstract class A
+    {
+        public void display()
+        {
+            Console.WriteLine("Non abstract method");
+        }
+        public abstract void absmethod();
+
+    }
+    class B : A
+    {
+        public override void absmethod()
+        {
+            Console.WriteLine("Abstract method");
+        }
+        public static void Main()
+        {
+            B obj = new B();
+            obj.display();
+            obj.absmethod();
+            Console.ReadKey();
+        }
+    }
+}
