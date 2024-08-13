429c0167cabf95c3ee822f5365ec22c309298722
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-13 13:45:22 +0530

    13/08/2024

---
+++ constructor/constructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    class A
+    {
+        public A()//default constructor
+        {
+            Console.WriteLine("Default constructor");
+        }
+        public A(int a,int b)//parameterized constructor
+        {
+            Console.WriteLine("Sum is:{0}", a + b);
+        }
+        static A()//static constructor
+        {
+            Console.WriteLine("Static constructor");
+        }
+        public static void Main()
+        {
+            A obj = new A();
+            A obj1 = new A(2,3);
+            Console.ReadKey();
+        }
+    }
+}
