6394a481e603a22504d45f63817659546dcafc0c
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-13 14:10:56 +0530

    13/08/2024

---
+++ property_read_write/property_read_write/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace property_read_write
+{
+    class A
+    {
+        private int x;
+        public int p1
+        {
+            set
+            {
+                x = value;
+            }
+            get
+            {
+                return x;
+            }
+        }
+    }
+    class B
+    {
+        public static void Main()
+        {
+            A obj = new A();
+            obj.p1 = 100;//set
+            Console.WriteLine("x={0}", obj.p1);//get
+            Console.ReadKey();
+        }
+    }
+}
