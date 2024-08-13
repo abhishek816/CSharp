fd55a02736c59eabca59fd65b72c07ff6449fa71
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-13 13:32:02 +0530

    13/08/2024

---
+++ sealed_class/sealed_class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sealed_class
+{
+    sealed class A
+    {
+        public void display()
+        {
+            Console.WriteLine("Sealed class method");
+        }
+    }
+    class B
+    {
+        public static void Main()
+        {
+            A obj = new A();
+            obj.display();
+            Console.ReadKey();
+        }
+    }
+}
