49e7993170926f8f2ba246bf3fc650a9977361c8
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-13 14:25:51 +0530

    13/08/2024

---
+++ property_automatic/property_automatic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace property_automatic
+{
+    class A
+    {
+        private int x;
+        public int p1
+        {
+            set;get;
+        }
+    }
+    class B
+    {
+        public static void Main()
+        {
+            A obj = new A();
+            obj.p1 = 100;//set
+            Console.WriteLine("x={0}", obj.p1);//get
+            Console.ReadKey();
+        }
+    }
+}
