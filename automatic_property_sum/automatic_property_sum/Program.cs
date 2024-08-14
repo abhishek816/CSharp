8719b2b811fabcec19721f47f206015b01a6e6b4
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-14 13:18:30 +0530

    14/08/2024

---
+++ automatic_property_sum/automatic_property_sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automatic_property_sum
+{
+    class A
+    {
+        public int a
+        {
+            get;
+            set;
+        }
+        public int b
+        {
+            get;
+            set;
+        }
+        public static void Main()
+        {
+            A obj = new A();
+            Console.WriteLine("Enter two numbers:");
+            obj.a = Convert.ToInt32(Console.ReadLine());
+            obj.b = Convert.ToInt32(Console.ReadLine());
+            int s = obj.a + obj.b;
+            Console.WriteLine("Sum is :{0}", s);
+            Console.ReadKey();
+        }
+    }
+    
+}
