827a783ec5f0b432c23aa41b5424e5eb6fdea9c4
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-16 14:25:16 +0530

    14/08/2024

---
+++ lambda_expression/lambda_expression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda_expression
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = new List<int>() { 12, 52, 41, 87, 69, 41 };
+            Console.WriteLine("The list:");
+            foreach (var v in numbers)
+            {
+                Console.Write("{0} ", v);
+            }
+            Console.WriteLine();
+            var square = numbers.Select(x => x * x);
+            Console.Write("Squares: ");
+            foreach(var value in square)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
