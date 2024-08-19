ba079635a6dc13a44035ebf545372a970bd52f0a
abhishek816 <abhishekbkumar321@gmail.com>
2024-08-19 13:16:49 +0530

    19/08/2024

---
+++ single_threading/single_threading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace single_threading
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Thread ob = new Thread(new ThreadStart(run));
+            ob.Start();
+            Console.ReadKey();
+        }
+        public static void run()
+        {
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine("Thread is:{0}", i);
+            }
+        }
+    }
+}
